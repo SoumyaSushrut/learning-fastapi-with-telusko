1fa11663ab31fa7d5f1395739cba5c736b7be45a
SoumyaSushrut <iitmsushrut@gmail.com>
2026-05-03 02:43:25 +0530

    imported pydantic

--- main.py
+++ main.py
@@ -8,12 +8,28 @@ app = FastAPI()
 def greet():
     return "Welcom bot 192006"
 
-products=[
-    Product(1,"mobile","budget mobile",99,10),
-    Product(2,"Laptop","Gaming laptop",999,6)
 
+products = [
+    Product(id=1, name="mobile", description="budget mobile", price=99, quantity=10),
+    Product(id=6, name="Laptop", description="Gaming laptop", price=999, quantity=6),
 ]
+
 
 @app.get("/products")
 def get_all_products():
     return products
+
+
+@app.get("/product/{id}")
+def get_product_by_id(id: int):
+    for product in products:
+        if product.id == id:
+            return product
+
+    return "product not found"
+
+
+@app.post("/product")
+def add_product(product: Product):
+    products.append(product)
+    return product
